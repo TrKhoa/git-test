717502c78557332a3344fec1539b64c1a443213a
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-02-22 14:56:26 +0700

    update

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,18 +1,58 @@
-import './App.css';
-import { Navbar,NavbarBrand } from 'reactstrap';
-import React from 'react';
+import { Card,CardTitle,CardText } from 'reactstrap';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import dateFormat from 'dateformat';
 
-function App() {
+class StaffList extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      selectedStaff: null
+    }
+  }
+
+  onStaffSelect(staff) {
+    this.setState({ selectedStaff: staff});
+  }
+
+  renderStaff(staff) {
+    if(staff!=null){
+      return(
+      <Card outline body color="primary" className="col-12 text-left">
+        <CardTitle tag="h2">{staff.name}</CardTitle>
+        <CardText tag="h4">
+          Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}<br />
+          Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}<br />
+          Phòng ban: {staff.department.name}<br />
+          Số ngày nghỉ còn lại: {staff.annualLeave}<br />
+          Số ngày đã làm thêm: {staff.overTime}
+        </CardText>
+      </Card>);
+    }
+  }
+  render(){
+    const menu = this.props.staffs.map((staff) => {
+      return (
+        <div className="col-12 col-lg-3 col-md-5 m-1">
+          <Card key={staff.id} onClick={() => this.onStaffSelect(staff)}>
+            <CardTitle>{staff.name}</CardTitle>
+          </Card>
+        </div>
+      );
+    });
   return (
-    <div className="App">
-    <Navbar dark color="primary">
-      <div className="container">
-        <NavbarBrand href="#">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+    <div className="container">
+       <div className="row">
+        { menu }
+       </div>
+      <div className="row">
+        <div  className="col-12 col-md-5 m-1">
+          {this.renderStaff(this.state.selectedStaff)}
+        </div>
       </div>
-    </Navbar>
-    </div>
-  );
+     </div>
+  );}
 }
 
-export default App;
+export default StaffList;
