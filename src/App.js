edb0b5f4de58cb743dee8320f207c720f2bd6c79
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-02-22 14:51:30 +0700

    app basic

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { Navbar,NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { STAFFS } from './shared/staffs';
+import StaffList from './components/StaffListComponent';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs: STAFFS
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="#">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+        <StaffList staffs={this.state.staffs} />
+      </div>
+    );
+  }
+}
+
+export default App;
