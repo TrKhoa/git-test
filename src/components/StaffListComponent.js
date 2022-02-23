bdb48109d448df6378a7d3c5bcfa8fe009ec29ea
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-02-23 13:32:22 +0700

    display button

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -20,16 +20,16 @@ class StaffList extends Component {
   handleColumn(prop){
     switch (prop) {
       case 1:
-        this.setState({ selectedColumnNumber: "col-12"})
+        this.setState({ selectedColumnNumber: "col-md-12"})
         break;
       case 2:
-        this.setState({ selectedColumnNumber: "col-5"})
+        this.setState({ selectedColumnNumber: "col-md-5"})
         break;
       case 3:
-        this.setState({ selectedColumnNumber: "col-3"})
+        this.setState({ selectedColumnNumber: "col-md-3"})
         break;
       default:
-        this.setState({ selectedColumnNumber: "col-12 col-lg-3 col-md-5"})
+        this.setState({ selectedColumnNumber: "col-lg-3 col-md-5"})
     }
   }
   renderStaff(staff) {
@@ -50,7 +50,7 @@ class StaffList extends Component {
   render(){
     const menu = this.props.staffs.map((staff) => {
       return (
-        <div id="item" className={ this.state.selectedColumnNumber +" m-1"}>
+        <div id="item" className={ "col-12 "+this.state.selectedColumnNumber +" m-1"}>
           <Card key={staff.id} onClick={() => this.onStaffSelect(staff)}>
             <CardTitle>{staff.name}</CardTitle>
           </Card>
@@ -59,7 +59,7 @@ class StaffList extends Component {
     });
   return (
     <div className="container">
-    <ButtonGroup className="float-right">
+    <ButtonGroup className="float-right d-none d-md-block">
       <Button onClick={ () => {this.handleColumn(0)}}>Default</Button>
       <Button onClick={ () => {this.handleColumn(1)}}>1</Button>
       <Button onClick={ () => {this.handleColumn(2)}}>2</Button>
