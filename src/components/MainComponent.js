d2797ceb8a23fab48e7cb02c674634392901e4a2
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-03-04 16:26:29 +0700

    commit

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+import Header from './HeaderComponent';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -19,16 +24,22 @@ class Main extends Component {
   }
 
   render() {
+    const HomePage = () => {
+        return(
+            <Home />
+        );
+      }
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-      </div>
+        <Footer />
+      </React.Fragment>
     );
   }
 }
