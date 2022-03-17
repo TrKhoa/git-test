b7a94eb9586d6446a9d53b234d5be667ca57b423
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-03-17 11:07:03 +0700

    done

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import { CardGroup,Card,CardBody,CardTitle,CardSubtitle,CardText,CardImg} from 'reactstrap';
+import { Loading } from './LoadingComponent';
 
-function RenderCard({data}){
-  return(
-    <Card className="m-1">
-      <CardImg src={data.image} />
-      <CardTitle>{data.name}</CardTitle>
-      {data.designation ? <CardSubtitle>{data.designation}</CardSubtitle> : null }
-      <CardText>{data.description}</CardText>
-    </Card>
-  )
+function RenderCard({data, isLoading, errMess}) {
+
+    if (isLoading) {
+        return(
+              <Loading />
+        );
+    }
+    else if (errMess) {
+        return(
+              <h4>{errMess}</h4>
+        );
+    }
+    else
+        return(
+          <CardGroup>
+          <Card>
+              <CardImg src={data.dish.image} alt={data.dish.name} />
+              <CardBody>
+              <CardTitle>{data.dish.name}</CardTitle>
+              {data.dish.designation ? <CardSubtitle>{data.dish.designation}</CardSubtitle> : null }
+              <CardText>{data.dish.description}</CardText>
+              </CardBody>
+          </Card>
+          <Card>
+              <CardImg src={data.promotion.image} alt={data.promotion.name} />
+              <CardBody>
+              <CardTitle>{data.promotion.name}</CardTitle>
+              {data.promotion.designation ? <CardSubtitle>{data.promotion.designation}</CardSubtitle> : null }
+              <CardText>{data.promotion.description}</CardText>
+              </CardBody>
+          </Card>
+          <Card>
+              <CardImg src={data.leader.image} alt={data.leader.name} />
+              <CardBody>
+              <CardTitle>{data.leader.name}</CardTitle>
+              {data.leader.designation ? <CardSubtitle>{data.leader.designation}</CardSubtitle> : null }
+              <CardText>{data.leader.description}</CardText>
+              </CardBody>
+          </Card>
+          </CardGroup>
+        );
+
 }
 
 export default function Home(props) {
@@ -18,9 +52,7 @@ export default function Home(props) {
       <div className="row">
         <h4>Home</h4>
         <CardGroup>
-          <RenderCard data={props.dish} />
-          <RenderCard data={props.promotion} />
-          <RenderCard data={props.leader} />
+          <RenderCard data={props} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
         </CardGroup>
       </div>
       </div>
