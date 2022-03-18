569bc93c33513f33878bf0c341490143ba54b59c
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-03-18 16:54:52 +0700

    VERSION 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import { CardGroup,Card,CardBody,CardTitle,CardSubtitle,CardText,CardImg} from 'reactstrap';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
-function RenderCard({data}){
-  return(
-    <Card className="m-1">
-      <CardImg src={data.image} />
-      <CardTitle>{data.name}</CardTitle>
-      {data.designation ? <CardSubtitle>{data.designation}</CardSubtitle> : null }
-      <CardText>{data.description}</CardText>
-    </Card>
-  )
+function RenderCard({data, isLoading, errMess}) {
+  console.log(isLoading)
+      if (isLoading) {
+       return(
+           <div className="container">
+               <div className="row">
+                   <Loading />
+               </div>
+           </div>
+       );
+     }
+      else
+        return(
+          <Card>
+              <CardImg src={data.image} alt={data.name} />
+              <CardBody>
+              <CardTitle>{data.name}</CardTitle>
+              {data.designation ? <CardSubtitle>{data.designation}</CardSubtitle> : null }
+              <CardText>{data.description}</CardText>
+              </CardBody>
+          </Card>
+        );
+
 }
 
 export default function Home(props) {
     return(
       <div className="container">
-      <div className="row">
+      <div className="row">{console.log(props)}
         <h4>Home</h4>
         <CardGroup>
-          <RenderCard data={props.dish} />
-          <RenderCard data={props.promotion} />
-          <RenderCard data={props.leader} />
+          <RenderCard data={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
+          <RenderCard data={props.promotion} isLoading={props.promosLoading} errMess={props.promosErrMess}  />
+          <RenderCard data={props.leader} isLoading={props.leadersLoading} errMess={props.promosErrMess}  />
         </CardGroup>
       </div>
       </div>
---
+++ src/redux/comments.js
@@ -0,0 +1,24 @@
+import * as ActionTypes from './ActionTypes';
+
+export const Comments = (state ={
+    isLoading: true,
+    errMess: null,
+    comments:[]}, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_COMMENTS:
+            return {...state, isLoading: false, errMess: null, comments: action.payload};
+
+        case ActionTypes.COMMENTS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+
+        case ActionTypes.ADD_COMMENT:
+            var comment = action.payload;
+            comment.id = state.comments.length;
+            comment.date = new Date().toISOString();
+            console.log("Comment: ", comment);
+            return {...state ,comment : state.comments.concat(comment)}
+
+        default:
+          return state;
+      }
+};
